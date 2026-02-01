be9c7f3f8e766364381f804f93f93bb2dc7896b6
Saksham Lamichhane <sakshamlamichhane456@gmail.com>
2026-02-01 08:57:35 +0545

    fix: include global types in tsconfig

---
+++ globals.d.ts
@@ -0,0 +1,13 @@
+export {}; 
+
+declare global {
+  interface Window {
+    solana?: {
+      isPhantom?: boolean;
+      connect: (options?: { onlyIfTrusted?: boolean }) => Promise<{ publicKey: { toString: () => string } }>;
+      disconnect: () => Promise<void>;
+      on: (event: string, callback: () => void) => void;
+      publicKey?: { toString: () => string };
+    };
+  }
+}
